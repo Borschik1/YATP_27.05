574b1aac6ea2a92e1df3d0e3f140f03f6c227263
Borschik <47531847+Borschik1@users.noreply.github.com>
2022-05-27 19:34:36 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,73 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+#include <queue>
+#include <cstdlib>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <cassert>
+
+using namespace std;
+
+
+void move(vector<vector<int>>& vec, vector<int>& plug, int num, int path) {
+    for (int i = 0; i < plug.size(); ++i) {
+        if (vec[i][num]) {
+            if (plug[i] > path + 1) {
+                plug[i] = path + 1;
+                move(vec, plug, i, path++);
+            }
+        }
+    }
+}
+
+
+int main() {
+    ifstream fin("D:/Desktop/INPUT.txt");
+    ofstream fout("D:/Desktop/OUTPUT.txt");
+    
+    int n, vBegin, vEnd;
+    fin >> n;
+    vector <vector<int>> a(n, vector<int>(n));
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            fin >> a[i][j];
+    fin >> vBegin >> vEnd;
+    vBegin--;
+    vEnd--;
+    const int UNDEF = -1;
+    vector <int> len(n, UNDEF);
+    queue <int> q;
+    q.push(vBegin);
+    len[vBegin] = 0;
+    while (true)
+    {
+        if (q.empty())
+        {
+            fout << -1;
+            return 0;
+        }
+        int cur = q.front();
+        q.pop();
+        if (cur == vEnd)
+        {
+            fout << len[vEnd];
+            return 0;
+        }
+        for (int next = 0; next < n; ++next)
+            if (len[next] == UNDEF && a[cur][next] == 1)
+            {
+                q.push(next);
+                len[next] = len[cur] + 1;
+            }
+    }
+
+    fin.close();
+    fout.close();
+    std::system("pause");
+    return 0;
+}
+
